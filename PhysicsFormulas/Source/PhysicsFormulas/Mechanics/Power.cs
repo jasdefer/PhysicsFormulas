d9518c66cc4881fd789486f2ed435887c95e9f41
Justus Bonz <justus.bonz@uni-hamburg.de>
2020-02-28 10:57:48 +0100

    Added work and power calculation
    See issue #8

---
+++ PhysicsFormulas/Source/PhysicsFormulas/Mechanics/Power.cs
@@ -0,0 +1,16 @@
+﻿namespace PhysicsFormulas.Mechanics
+{
+    public static class Power
+    {
+        /// <summary>
+        /// Calculate the rate of doing work per unit of time.
+        /// </summary>
+        /// <param name="work">The work done.</param>
+        /// <param name="duration">The duration.</param>
+        /// <returns>Calculates the value of power.</returns>
+        public static double FromWork(double work, double duration)
+        {
+            return work / duration;
+        }
+    }
+}
---
+++ PhysicsFormulas/Test/PhysicsFormulasUnitTests/MechanicsUnitTests/PowerUnitTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PhysicsFormulas.Mechanics;
+
+namespace PhysicsFormulasUnitTests.MechanicsUnitTests
+{
+    [TestClass]
+    public class PowerUnitTest
+    {
+        [TestMethod]
+        public void FromWork()
+        {
+            var p = Power.FromWork(3, 2);
+            Assert.AreEqual(1.5, p);
+        }   
+    }
+}
